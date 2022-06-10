4b8206adf90e3b0d10631b3f1b98953db1a1f153
nick quinn <nick.quinn@stadium1.com>
2022-06-10 12:38:21 -0400

    found an issue with package json script having an issue

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const {Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+const User = model("User", userSchema);
+
+module.exports = User;
